c48f8ebc7e49bf7effd7e21e4591429b18460790
friedmanrivka <hand1c26@mbjcomp.org.il>
2024-07-01 20:35:37 +0300

    upload

--- volunteers/volunteers-service.ts
+++ volunteers/volunteers-service.ts
@@ -4,7 +4,8 @@ import { Volunteer } from "../utils/type";
 export default class VolunteersService {
     constructor(private volunteersDal: VolunteersDal) {}
     public async createVolunteer(data: Volunteer): Promise<Volunteer> {
-        console.log('service')
+      
         return this.volunteersDal.createVolunteer(data);
+        
     }
 }
